4ff8e9e1b4095d647ae59ee043c4c532fcc641c3
Oleg Laktionov <ohlegik@gmail.com>
2020-06-06 14:54:37 +0300

    fix int32 overflow

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 func main() {
-	var balance int32 = 1_500_000_000       // 15 миллионов в копейках
-	var transfer int32 = 1_000_000_000      // 10 миллионов в копейках
-	total := balance + transfer // int32 + int32 будет int32
+	var balance uint32 = 1_500_000_000       // 15 миллионов в копейках
+	var transfer uint32 = 1_000_000_000      // 10 миллионов в копейках
+	total := balance + transfer // uint32 + uint32 будет int32
 	println(total)
 }
